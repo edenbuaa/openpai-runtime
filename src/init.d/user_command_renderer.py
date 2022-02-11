75f3e654e96cc49940cf079d40e70fc8555c8559
edenbuaa <edenbuaa@outlook.com>
2022-02-11 11:28:30 +0800

    Update user_command_renderer.py
    u.reserver.fix

--- src/init.d/user_command_renderer.py
+++ src/init.d/user_command_renderer.py
@@ -53,7 +53,7 @@ def main():
     LOGGER.info("not rendered user command is %s", user_command)
     
      # for debug.fly
-    isDebug_command ="echo \"======   The job container failed, so it will be reserved for 1 week   ======\""
+    isDebug_command ="echo \"======   The job container failed, so it will be reserved for 30 minutes   ======\""
     
     rendered_user_command = render_string_with_secrets(user_command, secrets)
     _output_user_command(rendered_user_command, args.output_file)
