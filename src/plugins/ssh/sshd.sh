61f9eb4ca2d5d6d6c88dfc8e2a4b42d2310b27ca
edenbuaa <edenbuaa@outlook.com>
2022-08-02 13:53:55 +0800

    Update sshd.sh
    add support for centos

--- src/plugins/ssh/sshd.sh
+++ src/plugins/ssh/sshd.sh
@@ -15,8 +15,19 @@
 # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# add support for centos
+# if os is centos bypass the pre-command and plugins
+isCentos=false
+if [ -f "/etc/redhat-release" ];then
+	if [[ `cat /etc/redhat-release` =~ "CentOS" ]];then
+		echo 'centos'
+    isCentos=true		
+	fi
+fi
+if [ "$isCentos" = false ];then
+  set -o errexit
+fi
 
-set -o errexit
 set -o nounset
 set -o pipefail
 
