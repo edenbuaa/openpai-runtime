52230a252f4cd078d1657529a0ef91cbc126b889
edenbuaa <edenbuaa@outlook.com>
2022-08-04 16:55:18 +0800

    Add files via upload

---
+++ src/plugins/filebrowser/filebrowser.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+PAI_WORK_DIR=/usr/local/pai
+
+echo "start filebrowser"
+
+tar -xzf ${PAI_WORK_DIR}/plugins/filebrowser/linux-amd64-filebrowser.tar.gz -C ${PAI_WORK_DIR}/plugins/filebrowser
+
+${PAI_WORK_DIR}/plugins/filebrowser/filebrowser config set --auth.method=noauth
+
+nohup ${PAI_WORK_DIR}/plugins/filebrowser/filebrowser -p $SFE_WEB_FILEBROWSER_PORT -a 0.0.0.0 -d ${PAI_WORK_DIR}/plugins/filebrowser -r /mnt  bash > /var/log/filebrowser.log 2>&1 &
