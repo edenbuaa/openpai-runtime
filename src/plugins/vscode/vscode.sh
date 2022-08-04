16be1c4afae3a4ec1e4dc1f5fefad5019901289d
edenbuaa <edenbuaa@outlook.com>
2022-08-04 18:33:00 +0800

    Update vscode.sh

--- src/plugins/vscode/vscode.sh
+++ src/plugins/vscode/vscode.sh
@@ -9,5 +9,5 @@ PAI_WORK_DIR=/usr/local/pai
 echo "start vscode"
 
 dpkg -i ${PAI_WORK_DIR}/plugins/vscode/code-server_4.5.1_amd64.deb
-
+rm -f ${PAI_WORK_DIR}/plugins/vscode/code-server_4.5.1_amd64.deb
 nohup code-server --host 0.0.0.0 --user-data-dir=/usr --port $SFE_WEB_VSCODE_PORT bash > /var/log/vscode.log 2>&1 &
