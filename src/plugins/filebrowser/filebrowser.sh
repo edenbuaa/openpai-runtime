cb3c91200cdc5f6bdbb10f5b6f8b44318da0269a
edenbuaa <edenbuaa@outlook.com>
2022-08-05 13:47:12 +0800

    Update filebrowser.sh

--- src/plugins/filebrowser/filebrowser.sh
+++ src/plugins/filebrowser/filebrowser.sh
@@ -16,6 +16,8 @@ ${PAI_WORK_DIR}/plugins/filebrowser/filebrowser config init
 
 ${PAI_WORK_DIR}/plugins/filebrowser/filebrowser users add sfe sfe
 
-${PAI_WORK_DIR}/plugins/filebrowser/filebrowser config set --auth.method=noauth
+${PAI_WORK_DIR}/plugins/filebrowser/filebrowser config set --auth.method=noauth --perm.download=false --locale=zh-cn
+
+${PAI_WORK_DIR}/plugins/filebrowser/filebrowser users update sfe --perm.download=false --locale=zh-cn
 
 nohup ${PAI_WORK_DIR}/plugins/filebrowser/filebrowser -p $SFE_WEB_FILEBROWSER_PORT -a 0.0.0.0 -d ${PAI_WORK_DIR}/plugins/filebrowser -r /mnt -b /filebrowser/svc-$FC_POD_NAME  &
