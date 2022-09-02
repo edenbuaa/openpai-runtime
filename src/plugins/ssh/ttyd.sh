9cbf8db37428127862bed5bef6df0cde7cc86331
edenbuaa <edenbuaa@outlook.com>
2022-09-02 15:47:05 +0800

    Update ttyd.sh
    add tty for every instance

--- src/plugins/ssh/ttyd.sh
+++ src/plugins/ssh/ttyd.sh
@@ -7,5 +7,6 @@ set -o pipefail
 PAI_WORK_DIR=/usr/local/pai
 
 echo "start ttyd"
-
-nohup ${PAI_WORK_DIR}/plugins/ssh/ttyd -t fontSize=16 -p $SFE_WEB_TERMINAL_PORT bash > /var/log/ttyd.log 2>&1 &
+ttyPortName=SFE_WEB_TERMINAL_PORT_${FC_TASKROLE_NAME}_${FC_TASK_INDEX}
+eval ttyPort=\${${ttyPortName}}
+nohup ${PAI_WORK_DIR}/plugins/ssh/ttyd -t fontSize=16 -p $ttyPort bash > /var/log/ttyd.log 2>&1 &
